5495d77f7dd626b4f8cfac895c4a64976e826166
leonimurilo <leonimega@hotmail.com>
2017-07-23 17:00:50 -0300

    turn Search bar component into a class based component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,9 +5,26 @@ import React from "react";
 // Needs to be imported because when a jsx tag is used, under the hood what is happening is that
 // it turns the tag into a call like: React.createElement
 
-const SearchBar = () => {
-    return <input/>;
-};
 
-//
+// // functional component
+// // something goes in and some jsx goes out
+// const SearchBar = () => {
+//     return <input/>;
+// };
+
+// class based component (ES6 class. An object with methods and properties)
+// It's used whenever you want a component to have some type of internal record
+// It has the ability to be aware of itself, what is happening to it since it has been rendered
+
+// Uses prototype inheritance to have all the properties and methods a component should have
+// every react class based component must have its render method
+class SearchBar extends React.Component{
+    render(){
+        return (
+            <input/>
+        );
+    }
+}
+
+
 export default SearchBar;

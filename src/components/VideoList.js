a4931c9a33c4f519d94e0e68777d79e0e195d6b7
leonimurilo <leonimega@hotmail.com>
2017-07-24 20:18:56 -0300

    fetch videos from youtube API

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,3 +1,15 @@
 /**
  * Created by Leoni on 7/23/2017.
  */
+import React from "React";
+
+const VideoList = (props) => {
+    const videos = props.videos;
+    return (
+        <ul className="col-md-4 list-group">
+            {props.videos.length}
+        </ul>
+    );
+};
+
+export default VideoList;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,46 @@
-import React from "react"; // core react library that knows how to work with components
-import ReactDOM from "react-dom" // separated lib that is responsible to insert the component into the DOM
-import SearchBar from "./components/SearchBar"
+import React, {Component} from "react"; // core react library that knows how to work with components
+import YTSearch from "youtube-api-search";
+import ReactDOM from "react-dom"; // separated lib that is responsible to insert the component into the DOM
+
+import SearchBar from "./components/SearchBar";
+import VideoList from "./components/VideoList"
+
+import apiKey from "../apiKey";
+
+const API_KEY = apiKey.youtube.API_KEY;
+
 
 // create a new class of a component (not an instance) that should produce some html
-const App = () => {
-    return (
-        <div>
-            <SearchBar/>
-        </div>
-    );
-};
+class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {videos: []};
+
+        YTSearch({key: API_KEY, term: "evanescence"}, videos => {
+            console.log(videos);
+            this.setState({videos});
+        });
+
+    }
+
+    // we can pass data from the parent component (app) to the child just by
+    // defining a property on the jsx tag
+    // passing data like this is referred to as passing the props of a component
+    // in a functional component, props comes as an argument
+    // in a class based component, props can be accessible from anywhere as this.props
+
+    render(){
+        return (
+            <div>
+                <SearchBar/>
+                <VideoList videos={this.state.videos}/>
+            </div>
+        );
+    }
+
+}
+
 
 
 // take the component and put it on the page (in the DOM)
